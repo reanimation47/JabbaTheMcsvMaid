30dbeb68c7dbbfafdb44c7461d4aeab06fedb15c
reanimation47 <reanimation47@gmail.com>
2022-04-14 10:04:34 +0700

    added closing connection for playercount

--- commands/mc-playercount.ts
+++ commands/mc-playercount.ts
@@ -9,12 +9,14 @@ import { randomEmotes } from "../jsons/lists"
 module.exports = {
     name: 'mc player count',
     async execute(message) {
+        var conn = new Rcon('mc-server.ldaq.org', 25575, process.env.RCON_PASS);
+        conn.connect()
         let mes: string = message.content.toLowerCase()
         let mess: string = removeVN(mes)
         if (message.author.id === "939491082717249558") return;
         if (mess == 'jabba server') {
             message.channel.send(`${getRandom(randomEmotes)} jabba is retrieving server's data `)
-            var conn = new Rcon('mc-server.ldaq.org', 25575, process.env.RCON_PASS);
+            
 
             conn.on('auth', function () {
                 // You must wait until this event is fired before sending any commands,
@@ -25,14 +27,16 @@ module.exports = {
             }).on('response', function (str) {
                 console.log("Response: " + str);
                 message.channel.send(` ${getRandom(randomEmotes)} The server is Online \n${str}`)
+                conn.disconnect()
             }).on('error', function (err) {
                 console.log("Error: " + err);
                 message.channel.send(`${getRandom(randomEmotes)} The server seems to be Offline.`)
+                conn.disconnect()
             }).on('end', function () {
                 console.log("Connection closed");
             });
 
-            conn.connect()
+            
         }
     }
 }
